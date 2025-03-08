782166f5e2c7f94de98ec45742ebbc567fa2aa69
rrhmn <raissarahmani18@gmail.com>
2025-03-08 16:32:08 +0700

    add: script js for signup page

---
+++ script/signup.js
@@ -0,0 +1,86 @@
+const email = document.querySelector("input[name='email']")
+const pass = document.querySelector("input[name='password']")
+const tnc = document.getElementById("tnc")
+const btnRegister = document.querySelector("form button")
+const btnMsg = document.querySelector("form p:last-of-type")
+
+btnRegister.addEventListener("click", (e) => {
+    e.preventDefault()
+    
+    const emailMsg = document.querySelector("form p:first-of-type")
+    const passMsg = document.querySelector("form p:nth-of-type(2)")
+
+    if (email.value == 0) {
+        emailMsg.style.visibility = "visible"
+        emailMsg.textContent = "Data must be filled"
+    } else {
+        emailMsg.style.visibility = "hidden"
+    }
+
+    if (pass.value == 0) {
+        passMsg.style.visibility = "visible"
+        passMsg.textContent = "Data must be filled"
+        return
+    } else {
+        passMsg.style.visibility = "hidden"
+    }
+
+    if (!email.value.includes("@")) {
+        emailMsg.style.visibility = "visible"
+        emailMsg.textContent = "Email not valid"
+        return
+    }
+
+    if (pass.value.length < 8) {
+        passMsg.style.visibility = "visible"
+        passMsg.textContent = "Password should at least has 8 characters"
+        return
+    }
+
+    let alphabetPass = pass.value.toLowerCase() !== pass.value.toUpperCase()
+    if (alphabetPass === false) {
+        passMsg.style.visibility = "visible"
+        passMsg.textContent = "Password should contain letter"
+        return
+    }
+
+    let numPass = false
+    for (let num of pass.value) {
+        if (!isNaN(num)) {
+            numPass = true
+        }
+    }
+    if (!numPass) {
+        passMsg.style.visibility = "visible"
+        passMsg.textContent = "Password should contain number"
+        return
+    }
+
+    const tncMsg = document.querySelector("form p:nth-of-type(3)")
+    if (!tnc.checked) {
+        tncMsg.style.visibility = "visible"
+        return
+    }
+
+    btnMsg.style.visibility = "visible"
+    location.href = "signin.html"
+})
+
+const showPass = document.querySelector("form div img")
+showPass.addEventListener ("click", () => {
+    if (pass.type === "password") {
+        pass.type = "text"
+    } else {
+        pass.type = "password"
+    }
+})
+
+const loginGoogle = document.querySelector(".socmed div:first-of-type img")
+loginGoogle.addEventListener("click", () => {
+    location.href = "https://www.google.com/login"
+}, false)
+
+const loginFb = document.querySelector(".socmed div:last-of-type img")
+loginFb.addEventListener("click", () => {
+    location.href = "https://www.facebook.com/login"
+}, false)
