6835949d70607d9351852dea62e66475a158146f
rrhmn <raissarahmani18@gmail.com>
2025-03-09 14:38:09 +0700

    add: js script for admin movie table page

---
+++ script/admin2.js
@@ -0,0 +1,15 @@
+const toDashboard = document.querySelector("nav p:first-of-type")
+toDashboard.addEventListener("click", () => {
+    location.href = "admin.html"
+})
+
+const toMovie = document.querySelector("nav p:last-of-type")
+toMovie.addEventListener("click", () => {
+    location.href = "admin2.html"
+})
+
+const addMovie = document.querySelector("form div button")
+addMovie.addEventListener("click", (e) => {
+    e.preventDefault()
+    location.href = "admin3.html"
+})
