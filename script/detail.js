5c54095e242638816819e2d76a211837d4d6fe28
rrhmn <raissarahmani18@gmail.com>
2025-03-09 21:29:28 +0700

    add: hamburger menu

--- script/detail.js
+++ script/detail.js
@@ -13,13 +13,13 @@ toBuyTicket.addEventListener("click", () => {
     location.href = "detail.html"
 }, false)
 
-const signIn = document.querySelector(".buttons>button:first-of-type")
-signIn.addEventListener("click", () => {
+const toSignIn = document.querySelector(".buttons>button:first-of-type")
+toSignIn.addEventListener("click", () => {
     location.href = "signin.html"
 }, false)
 
-const signUp = document.querySelector(".buttons>button:last-of-type")
-signUp.addEventListener("click", () => {
+const toSignUp = document.querySelector(".buttons>button:last-of-type")
+toSignUp.addEventListener("click", () => {
     location.href = "signup.html"
 }, false)
 
@@ -71,3 +71,34 @@ btnBook.addEventListener("click", (e) => {
 
     location.href = "order.html"
 })
+
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+const mobileMenu = document.querySelector("header div:last-child img")
+mobileMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active")
+})
+
+const home = document.querySelector(".hamburger-menu>nav>p:first-of-type")
+home.addEventListener("click", () => {
+    location.href = "../index.html"
+}, false)
+
+const movie = document.querySelector(".hamburger-menu>nav>p:nth-of-type(2)")
+movie.addEventListener("click", () => {
+    location.href = "home2.html"
+}, false)
+
+const buyTicket = document.querySelector(".hamburger-menu>nav>p:nth-of-type(3)")
+buyTicket.addEventListener("click", () => {
+    location.href = "detail.html"
+}, false)
+
+const signIn = document.querySelector(".hamburger-menu>nav>p:nth-of-type(4)")
+signIn.addEventListener("click", () => {
+    location.href = "signin.html"
+}, false)
+
+const signUp = document.querySelector(".hamburger-menu>nav>p:nth-of-type(5)")
+signUp.addEventListener("click", () => {
+    location.href = "signup.html"
+}, false)
