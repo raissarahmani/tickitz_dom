b0c2a78c2dd4fa6e9b4109572e0efd8197eb273d
rrhmn <raissarahmani18@gmail.com>
2025-03-11 21:36:32 +0700

    add: fetch data from API

--- script/home2.js
+++ script/home2.js
@@ -23,6 +23,66 @@ toSignUp.addEventListener("click", () => {
     location.href = "signup.html"
 }, false)
 
+// fetch movie list
+const API_KEY = "ae266d92588cded8ace6fb5f85c35955"
+const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=1`
+const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+
+const containerNow = document.querySelector(".movie-now")
+let genreList = {}
+
+async function getGenre () {
+    try {
+        const response = await fetch (genreUrl)
+        const dataGenre = await response.json()
+        genreList = dataGenre.genres.reduce((acc, genre) => {
+            acc[genre.id] = genre.name
+            return acc
+        }, {})
+        movieList()
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+async function movieList () {
+    try {
+        const response = await fetch(nowPlayingUrl)
+        const dataMovie = await response.json()
+        displayNowPlaying(dataMovie.results.slice(0,12))
+    } 
+    catch (error) {
+        console.error (error.message)
+    }
+}
+
+function displayNowPlaying(movies) {
+    containerNow.innerHTML = ""
+    movies.forEach(movie => {
+        const movieContainer = document.createElement("div")
+        movieContainer.classList.add("container-now")
+
+        const genreNames = movie.genre_ids.map(id => genreList[id]).slice(0,2)
+
+        movieContainer.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <p class="movie-title">${movie.title}</p>
+            <div class="genre">
+                ${genreNames.map(name => `<p>${name}</p>`).join("")}
+            </div>
+            <div class="detail">
+                <div>Details</div>
+                <div>Buy Ticket</div>
+            </div>`
+        containerNow.appendChild(movieContainer)
+
+    })
+}
+
+getGenre()
+
+
+// to detail movie
 const detailMovie = document.querySelectorAll(".detail :first-child")
 detailMovie.forEach((detail) => {
     detail.addEventListener("click", () => {
